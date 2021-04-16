3072bfa5b447e2502fc0b5a6093a78bf2d84eb99
intentbi3@gmail.com <intentbi3@gmail.com>
2021-04-16 18:52:05 +0530

    few update on layout and added few components

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,40 +1,62 @@
-import { Container, Grid, Paper } from '@material-ui/core';
+import { Checkbox, Container, FormControlLabel, FormGroup, Grid, Paper } from '@material-ui/core';
 import React from 'react';
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme)=>({
+const useStyles = makeStyles((theme) => ({
     root: {
+        // height: "980vh",
+        // border: "1px solid",
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexGrow: 1,
-        padding:theme.spacing(1),
-        border:"1px solid",
+        // justifyContent: "center",
+        // alignItems:"center"
+    },
+    paper: {
+        // height: "50vh",
+        width: "100%",
+        padding: "1rem",
+        marginTop: "1rem"
     }
 }));
 
+
+
 const Home = () => {
     const classes = useStyles();
+    const [checked, setChecked] = React.useState(true)
     return (
-        <div className={classes.root}>
-            <Paper elevation={3} >
-            <Grid container direction="row" justify="center" alignItems="center">
+        <Container maxWidth="md" className={classes.root}>
+            <Paper className={classes.paper} elevation={3} >
+                <Grid container direction="row" justify="flex-start" spacing={2}>
 
-                <Grid container item justify="space-evenly">
-
-                    <Button variant="contained" color="primary" >
-                        Primary
-                    </Button>
-                    <Button variant="contained" color="secondary" >
-                        secondary
-                    </Button>
+                    <Grid item >
+                        <Button variant="contained" color="default" >default</Button>
+                    </Grid>
+                    <Grid item >
+                        <Button variant="contained" color="primary" >Primary</Button>
+                    </Grid>
+                    <Grid item >
+                        <Button variant="contained" color="secondary" >secondary</Button>
+                    </Grid>
 
                 </Grid>
+                <Grid container direction="row" justify="flex-start" spacing={2}>
 
-            </Grid>
+                    <Grid item >
+                        <FormGroup row>
+                            <FormControlLabel control={
+                                <Checkbox checked={checked} onChange={ (e,value)=>setChecked(value) } name="primary" color="primary" />
+                            }  label="primary"/>
+                            <FormControlLabel control={
+                                <Checkbox checked={checked} onChange={ (e,value)=>setChecked(value) } name="secondary" color="secondary" />
+                            }  label="secondary"/>
+                        </FormGroup>
+                    </Grid>
+
+
+                </Grid>
             </Paper>
-        </div >
+        </Container>
     );
 }
 
