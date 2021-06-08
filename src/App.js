77d1ebc96fb08cb123c0b14d2a2a0dfecfcfb4ac
sapthagiri <sapthagirirmsh8@gmail.com>
2021-06-08 23:31:55 +0530

    UI update, added navigation drawer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,181 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Home from './pages/home';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Home/>
-      </div>
-    );
-  }
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import BusinessCenterRoundedIcon from '@material-ui/icons/BusinessCenterRounded';
+import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import DraftsRoundedIcon from '@material-ui/icons/DraftsRounded';
+
+
+import Home from './pages/home';
+import About from './pages/About';
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+
+}));
+
+const App = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  return (
+    <Router className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Mini variant drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        {/* <Divider /> */}
+        <List>
+          <ListItem button key={0} component={Link} to="/">
+            <ListItemIcon><HomeRoundedIcon /></ListItemIcon>
+            <ListItemText primary={"Home"} />
+          </ListItem>
+          <ListItem button key={0} component={Link} to="/about">
+            <ListItemIcon><PersonRoundedIcon /></ListItemIcon>
+            <ListItemText primary={"About"} />
+          </ListItem>
+          <ListItem button key={0} component={Link} to="/portfolio">
+            <ListItemIcon><BusinessCenterRoundedIcon /></ListItemIcon>
+            <ListItemText primary={"Portfolio"} />
+          </ListItem>
+          <ListItem button key={0} component={Link} to="/contact">
+            <ListItemIcon><DraftsRoundedIcon /></ListItemIcon>
+            <ListItemText primary={"Contact"} />
+          </ListItem>
+        </List>
+      </Drawer>
+      
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,48 @@
+import { Container, Grid, Paper } from '@material-ui/core';
+import React from 'react';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        // height: "980vh",
+        // border: "1px solid",
+        display: "flex",
+        // justifyContent: "center",
+        // alignItems:"center"
+    },
+    paper: {
+        // height: "50vh",
+        width: "100%",
+        padding: "1rem",
+        marginTop: "1rem"
+    }
+}));
+
+
+// About page
+const About = () => {
+    const classes = useStyles();
+    const [checked, setChecked] = React.useState(true)
+    return (
+        <Container maxWidth="md" className={classes.root}>
+            <Paper className={classes.paper} elevation={3} >
+                <Grid container direction="row" justify="flex-start" spacing={2}>
+
+                    <Grid item >
+                        <Button variant="contained" color="default" >default</Button>
+                    </Grid>
+                    <Grid item >
+                        <Button variant="contained" color="primary" >Primary</Button>
+                    </Grid>
+                    <Grid item >
+                        <Button variant="contained" color="secondary" >secondary</Button>
+                    </Grid>
+
+                </Grid>
+            </Paper>
+        </Container>
+    );
+}
+
+export default About;
